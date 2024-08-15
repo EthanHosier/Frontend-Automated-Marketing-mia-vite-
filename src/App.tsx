ae38d23bdc42a835bb3ef06e031d957ac628eb01
Ethan Hosier <ethanjhosier@gmail.com>
2024-08-15 19:51:24 +0100

    feat: setup variables from figma

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-grayscaleText-negative bg-primary-surface-default">
+      idk
+    </div>
+  );
+};
+
+export default App;
