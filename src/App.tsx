cda8929266ec6946a6b61e55e1feaab6c16bb950
Ethan Hosier <ethanjhosier@gmail.com>
2024-08-16 21:26:59 +0100

    feAT: add text component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import Blob from "./components/blob";
-import { HStack } from "./components/hstack";
+import Text from "./components/text";
 
 const App = () => {
   return (
-    <div className="flex w-screen h-screen items-center justify-center bg-grayscaleSurface-default"></div>
+    <div className="flex w-screen h-screen items-center justify-center bg-grayscaleSurface-default">
+      <Text size="body" variant="regular">
+        idk
+      </Text>
+    </div>
   );
 };
 
---
+++ src/components/text.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+
+interface TextProps {
+  children: React.ReactNode;
+  color?: string;
+  size?:
+    | "h1"
+    | "h2"
+    | "h3"
+    | "h4"
+    | "headline"
+    | "body"
+    | "subtitle"
+    | "caption"
+    | "footnote";
+  variant?: "light" | "regular" | "bold";
+}
+
+const Text: React.FC<TextProps> = ({
+  children,
+  color,
+  size = "body",
+  variant = "regular",
+}) => {
+  const textClass = cn({
+    "text-h1-bold": size === "h1" && variant === "bold",
+    "text-h1-light": size === "h1" && variant === "light",
+    "text-h1-regular": size === "h1" && variant === "regular",
+    "text-h2-bold": size === "h2" && variant === "bold",
+    "text-h2-light": size === "h2" && variant === "light",
+    "text-h2-regular": size === "h2" && variant === "regular",
+    "text-h3-bold": size === "h3" && variant === "bold",
+    "text-h3-light": size === "h3" && variant === "light",
+    "text-h3-regular": size === "h3" && variant === "regular",
+    "text-h4-bold": size === "h4" && variant === "bold",
+    "text-h4-light": size === "h4" && variant === "light",
+    "text-h4-regular": size === "h4" && variant === "regular",
+    "text-headline-bold": size === "headline" && variant === "bold",
+    "text-headline-light": size === "headline" && variant === "light",
+    "text-headline-regular": size === "headline" && variant === "regular",
+    "text-body-bold": size === "body" && variant === "bold",
+    "text-body-regular": size === "body" && variant === "regular",
+    "text-body-light": size === "body" && variant === "light",
+    "text-subtitle-bold": size === "subtitle" && variant === "bold",
+    "text-subtitle-light": size === "subtitle" && variant === "light",
+    "text-subtitle-regular": size === "subtitle" && variant === "regular",
+    "text-caption-bold": size === "caption" && variant === "bold",
+    "text-caption-light": size === "caption" && variant === "light",
+    "text-caption-regular": size === "caption" && variant === "regular",
+    "text-footnote-bold": size === "footnote" && variant === "bold",
+    "text-footnote-light": size === "footnote" && variant === "light",
+    "text-footnote-regular": size === "footnote" && variant === "regular",
+  });
+
+  return (
+    <span className={textClass} style={{ color }}>
+      {children}
+    </span>
+  );
+};
+
+export default Text;
