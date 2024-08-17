63ecbfbc3158962a43ced54210897e4c759e5851
Ethan Hosier <ethanjhosier@gmail.com>
2024-08-17 12:33:10 +0100

    feat: add form stuff

--- src/components/text-input.tsx
+++ src/components/text-input.tsx
@@ -1,42 +1,60 @@
 import { cn } from "@/lib/utils";
+import { useFormContext } from "react-hook-form";
+import { VStack } from "./vstack";
+import Text from "./text";
 
 interface TextInputProps {
   label: string;
   type?: string;
   widthFull?: boolean;
-  required?: boolean;
+  name: string;
 }
 
 export const TextInput: React.FC<TextInputProps> = ({
   label,
   type = "text",
   widthFull,
-  required,
+  name,
+  ...props
 }) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+  const error = errors[name];
+
   return (
-    <div
-      className={cn(
-        "relative bg-grayscaleSurface-default rounded-lg w-[342px] h-[52px] hover:bg-gray-200 transition-colors duration-200 ease-in",
-        {
-          "w-full": widthFull,
-        }
-      )}
-    >
-      <div className="group relative z-0 mb-0 w-full flex items-center h-full px-4">
-        <input
-          type={type}
-          id="floating-input"
-          className="peer block w-full appearance-none bg-transparent px-0 text-sm text-grayscaleText-body focus:border-blue-600 focus:outline-none focus:ring-0 mt-1 h-full"
-          placeholder=" "
-          required={!!required}
-        />
-        <label
-          htmlFor="floating-input"
-          className="absolute -z-10 origin-[0] -translate-y-[14px] scale-75 transform text-sm text-grayscaleText-subtitle duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-[14px] peer-focus:scale-75 "
-        >
-          {label}
-        </label>
+    <VStack gap={1}>
+      <div
+        className={cn(
+          "relative bg-grayscaleSurface-default rounded-lg w-[342px] h-[52px] hover:bg-gray-200 transition-colors duration-200 ease-in",
+          {
+            "w-full": widthFull,
+          }
+        )}
+      >
+        <div className="group relative z-0 mb-0 w-full flex items-center h-full">
+          <input
+            type={type}
+            className="peer block w-full appearance-none bg-transparent text-sm text-grayscaleText-body focus:border-blue-600 focus:outline-none focus:ring-0 h-full px-4 rounded-lg pt-1"
+            placeholder=" "
+            {...register(name)}
+            {...props}
+          />
+          <label
+            htmlFor="floating-input"
+            className="ml-4 absolute -z-10 origin-[0] -translate-y-[14px] scale-75 transform text-sm text-grayscaleText-subtitle duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-[14px] peer-focus:scale-75 "
+          >
+            {label}
+          </label>
+        </div>
       </div>
-    </div>
+      {error && (
+        <Text variant="light" color="text-red-700" className="ml-4">
+          {" "}
+          {error.message?.toString()}
+        </Text>
+      )}
+    </VStack>
   );
 };
--- src/pages/auth/components/signUpForm.tsx
+++ src/pages/auth/components/signUpForm.tsx
@@ -1,18 +1,35 @@
 import { Button } from "@/components/button";
 import { Gap } from "@/components/gap";
 import { TextInput } from "@/components/text-input";
+import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
+import { SignUpFormData, signUpFormSchema } from "./signUpForm/types";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const SignUpForm = () => {
+  const methods = useForm<SignUpFormData>({
+    resolver: zodResolver(signUpFormSchema),
+  });
+  const { register, handleSubmit, setError } = methods;
+
+  const onSubmit: SubmitHandler<SignUpFormData> = (data) => {
+    console.log(data);
+  };
+
   return (
-    <>
-      <TextInput label="Name" type="text" widthFull required />
-      <TextInput label="Email" type="email" widthFull required />
-      <TextInput label="Password" type="password" widthFull required />
-      <Gap />
-      <Button variant="primary" size="xl">
-        Create Account
-      </Button>
-    </>
+    <FormProvider {...methods}>
+      <form
+        className="gap-inherit flex flex-col"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <TextInput name="name" label="Name" type="text" widthFull />
+        <TextInput name="email" label="Email" type="email" widthFull />
+        <TextInput name="password" label="Password" type="password" widthFull />
+        <Gap />
+        <Button variant="primary" size="xl">
+          Create Account
+        </Button>
+      </form>
+    </FormProvider>
   );
 };
 
